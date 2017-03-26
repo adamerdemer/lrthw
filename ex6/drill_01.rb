3d7acbcefa9df89245c9231cb10994c517b7eecf
Adam Erdem Er <adamerdemer@gmail.com>
2017-03-26 03:57:20 +0100

    ex6

---
+++ ex6/drill_01.rb
@@ -0,0 +1,29 @@
+#assigns value 10 to types_of_people
+types_of_people = 10
+# assigns types_of_people to variable
+x = "There are #{types_of_people} types of people."
+# assigns strings to two different variables
+binary = "binary"
+do_not = "don't"
+# assigns variable y as a string containing two other variables
+y = "Those who know #{binary} and those who #{do_not}."
+
+# prints x and y
+puts x
+puts y
+
+#prints strings with the values of x and y
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# assigns false value to variable
+hilarious = false
+# assigns value of hiliarous to joke_evaluation
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#prints value of joke_evaluation
+puts joke_evaluation
+# assigns two strings to two different variables
+w = "This is the left side of..."
+e = "a string with a right side."
+#prints the combined string value of w and e
+puts w + e
