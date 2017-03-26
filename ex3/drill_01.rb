d8b22e527a8b81a6da4a975be1fcac5fe1422a39
Adam Erdem Er <adamerdemer@gmail.com>
2017-03-26 02:47:15 +0100

    ex3

---
+++ ex3/drill_01.rb
@@ -0,0 +1,34 @@
+#prints string and adds blank line
+puts "I will now count my chickens:"
+
+#prints string and does the math
+puts "Hens #{25 + 30 / 6}"
+#prints string and does the math within the #{}
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#prints string and adds blank line
+puts "Now I will count the eggs:"
+
+#does the math operation, which is basically 1 + 4 % 2 - 1 /4 + 6
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#prints the string but doesnt do the mathetical operation becuase its not using #[]
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#does the math and prints if its true or false, in this case its false
+puts 3 + 2 < 5 - 7
+
+#unlike the other example, because this has #{} it pritns string and does the math
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+#prints string
+puts "Oh, that's why it's false."
+
+#prints string
+puts "How about some more."
+
+#prints string and the answer to the math
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
