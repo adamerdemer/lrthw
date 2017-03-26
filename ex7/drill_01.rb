49546a4cdd7d739bfcffae83119f109a17f4aa8f
Adam Erdem Er <adamerdemer@gmail.com>
2017-03-26 04:06:16 +0100

    ex7

---
+++ ex7/drill_01.rb
@@ -0,0 +1,26 @@
+#prints each string
+puts "Mary had a little lamb."
+#prints snow as a part of the string without the quotes
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+#prints 10 dots
+puts "." * 10  # what'd that do?
+#assigns 12 variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+#prints first 6 variables combined together without line breaker (print instead of puts)
+print end1 + end2 + end3 + end4 + end5 + end6
+#prints secon 6 variables combined together with line break (puts instead of print)
+puts end7 + end8 + end9 + end10 + end11 + end12
