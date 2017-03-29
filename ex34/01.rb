e13392ba667ae0786db72b1d24b4142322a266fb
Adam Erdem Er <adamerdemer@gmail.com>
2017-03-29 05:19:53 +0100

    ex34

---
+++ ex34/01.rb
@@ -0,0 +1,9 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "1. The 2nd animal is at 1 is a ruby"
+puts "2. The third animal is at 2 and is a peacock"
+puts "3. The first animal is at 0 and is a bear"
+puts "4. The 4th animal is at 3 and is a kangaroo"
+puts "5. The fifth animal is at 4 and is whale"
+puts "6. The sixth animal is at 5 and is a playtypus"
+puts "8. The 5th animal is at 4 and is a whale"
