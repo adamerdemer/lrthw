6e092d5afdc82129859735a9bf8db1dac86ec14e
Adam Erdem Er <adamerdemer@gmail.com>
2017-03-29 07:28:59 +0100

    ex38

---
+++ ex38/drill_06.rb
@@ -0,0 +1,10 @@
+#If you are someone that follows news about worldwide conflict, such as the Syrian Civil War, then you know how confusing it can be with the amount of groups there are.
+#This could give a better understanding of the situation.
+#This could be useful on a website like syrialivemap to give better understanding to the user of the situation
+
+syrian_allied_groups = ["as-sa'iqa", "Syrian Social Nationalist Party", "Arab Nationalist Guard", "Amal Movement", "Syrian Resistance", "Jaysh al-Muwahhideen", "Forces of Abu Ibrahim" "Liwa al-Jabal"]
+puts "These are some of the groups alliened with the Syrian Sovereign Goverment(SSA)"
+
+syrian_allied_groups.each do |ssa_allied|
+  puts "The#{ssa_allied} is an ally of the SSA"
+end
