077e1584b1d12328ff23c1c50357d4e1eed9e13a
Adam Erdem Er <adamerdemer@gmail.com>
2017-03-29 03:27:45 +0100

    ex21

---
+++ ex21/drill_03.rb
@@ -0,0 +1,8 @@
+puts "here is an example"
+
+first = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "This becomes : #{first}. This can be done in another way"
+
+second = add(age, subtract(height, multiply(weight, divide(iq, 2))
+puts  "changing the order of the arguemnts entered into subtract, you get #{second}"
